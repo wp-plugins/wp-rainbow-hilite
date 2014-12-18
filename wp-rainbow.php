a0e45fb5e0edb6f2f98e1c48a1795b56594ad5c0
podpirate <podpirate@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-18 13:30:58 +0000

    v1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-rainbow-hilite/tags/1.0.3@1048131 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-rainbow.php
+++ wp-rainbow.php
@@ -2,18 +2,18 @@
 
 /*
 Plugin Name: WordPress Rainbow Hilite
-Plugin URI: https://github.com/mcguffin/wp-rainbow-hilite
+Plugin URI: http://wordpress.org/plugins/wp-rainbow-hilite/
 Description: Code Syntax coloring using <a href="http://craig.is/making/rainbows">rainbow</a>.
 Author: Jörn Lund
-Version: 1.0.0
+Version: 1.0.3
 Author URI: https://github.com/mcguffin
 License: GPL2
 
-Text Domain: rainbow
+Text Domain: wp-rainbow-hilite
 Domain Path: /languages/
 */
 
-/*  Copyright 2014  Jörn Lund  (email : joern@podpirate.org)
+/*  Copyright 2014  Jörn Lund  (email : joern AT podpirate DOT org)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -62,7 +62,7 @@ class WPRainbow {
 		add_action( 'init' , array( &$this , 'init' ) );
 		add_filter( 'wp_kses_allowed_html' , array( &$this , 'allow_pre_tag' ) , 10 , 2 );
 
-		add_action( 'wp_enqueue_scripts' , array( &$this , 'enqueue_assets' ) );
+		add_action( 'wp_enqueue_scripts' , array( &$this , 'enqueue_assets' ) , 20 );
 		
 		add_option('wprainbow_load_minified' , true );
 		add_option('wprainbow_line_numbers' , false );
@@ -71,7 +71,7 @@ class WPRainbow {
 		
 	}
 	public function plugin_loaded() {
-		load_plugin_textdomain( 'rainbow' , false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+		load_plugin_textdomain( 'wp-rainbow-hilite' , false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 	/**
 	 * Adding pre tag and rainbow attributes to list of allowed tags
@@ -94,7 +94,6 @@ class WPRainbow {
 	/**
 	 * Init hook.
 	 * 
-	 *  - Load Textdomain
 	 *  - Register assets
 	 */
 	function init() {
@@ -161,30 +160,30 @@ class WPRainbow {
 	 *
 	 *	Function is not intended to be called directly. Use `wprainbow_get_available_languages()` instead.
 	 *
-	 *	@use private
+	 *	@use public
 	 *
 	 *	@return array Assoc containing all avaliable languages with language slugs as key and localized language Names as values. 
 	 */
 	function get_available_languages() {
 		$langs = array(
-			'c'				=> __('C','rainbow'),
-			'coffeescript'	=> __('coffeescript','rainbow'),
-			'csharp'		=> __('C#','rainbow'),
-			'css'			=> __('CSS','rainbow'),
-			'd'				=> __('D','rainbow'),
-			'go'			=> __('Go','rainbow'),
-			'haskell'		=> __('Haskell','rainbow'),
-			'html'			=> __('HTML','rainbow'),
-			'java'			=> __('Java','rainbow'),
-			'javascript'	=> __('JavaScript','rainbow'),
-			'lua'			=> __('Lua','rainbow'),
-			'php'			=> __('PHP','rainbow'),
-			'python'		=> __('Python','rainbow'),
-			'r'				=> __('R','rainbow'),
-			'ruby'			=> __('Ruby','rainbow'),
-			'scheme'		=> __('Scheme','rainbow'),
-			'shell'			=> __('Shell script','rainbow'),
-			'smalltalk'		=> __('Smalltalk','rainbow'),
+			'c'				=> __('C','wp-rainbow-hilite'),
+			'coffeescript'	=> __('coffeescript','wp-rainbow-hilite'),
+			'csharp'		=> __('C#','wp-rainbow-hilite'),
+			'css'			=> __('CSS','wp-rainbow-hilite'),
+			'd'				=> __('D','wp-rainbow-hilite'),
+			'go'			=> __('Go','wp-rainbow-hilite'),
+			'haskell'		=> __('Haskell','wp-rainbow-hilite'),
+			'html'			=> __('HTML','wp-rainbow-hilite'),
+			'java'			=> __('Java','wp-rainbow-hilite'),
+			'javascript'	=> __('JavaScript','wp-rainbow-hilite'),
+			'lua'			=> __('Lua','wp-rainbow-hilite'),
+			'php'			=> __('PHP','wp-rainbow-hilite'),
+			'python'		=> __('Python','wp-rainbow-hilite'),
+			'r'				=> __('R','wp-rainbow-hilite'),
+			'ruby'			=> __('Ruby','wp-rainbow-hilite'),
+			'scheme'		=> __('Scheme','wp-rainbow-hilite'),
+			'shell'			=> __('Shell script','wp-rainbow-hilite'),
+			'smalltalk'		=> __('Smalltalk','wp-rainbow-hilite'),
 		);
 		return apply_filters( 'wprainbow_available_languages' , $langs );
 	}
